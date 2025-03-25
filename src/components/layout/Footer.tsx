7bf63a2ff35faa2398edda255bad600f122b8dcb
Flleeppyy <flleeppyy@gmail.com>
2025-03-25 05:31:06 -0700

    feat: add github repo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 
 // Loosely ported from monkestation's uwuify.dm file
@@ -75,7 +76,7 @@ const Footer = () => {
     <footer className="bg-black/80 border-t border-gray-800 pb-8 mt-auto">
       <div className="container mx-auto">
         <div className="border-gray-800 pt-8 text-center text-gray-500">
-          <p>© {new Date().getFullYear()} Monkestation.</p>
+          <p>© {new Date().getFullYear()} Monkestation | <Link href="https://github.com/monkestation/game-hub">Github Repo</Link></p>
           <p className="mt-2 text-sm">
             Space Station 13 is a community-developed, multiplayer round-based role playing game, where players assume the role of a crewmember on a space station.
           </p>
